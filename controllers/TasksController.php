ea9bea3656b73d0519d025086a25f1594b4cdbcb
VK <belsynth@gmail.com>
2022-07-14 22:04:40 +0300

    Добавляет реализацию фильтрации в контроллер

--- controllers/TasksController.php
+++ controllers/TasksController.php
@@ -2,20 +2,72 @@
 
 namespace app\controllers;
 
+use app\models\Category;
+use app\models\Response;
 use app\models\Task;
+use app\models\TasksFilterForm;
 use Taskforce\Logic\Task as TaskLogic;
+use Yii;
+use yii\data\ActiveDataProvider;
+use yii\helpers\ArrayHelper;
 use yii\web\Controller;
 
 class TasksController extends Controller
 {
     public function actionIndex(): string
     {
-        $tasks = Task::find()
-            ->where(['status' => TaskLogic::STATUS_NEW])
-            ->with('city', 'category')
-            ->orderBy('created_at DESC')
-            ->all();
+        $this->view->title = 'Новые задания :: Taskforce';
 
-        return $this->render('index', compact('tasks'));
+        $categories = Category::find()->asArray()->all();
+        $categoriesList = ArrayHelper::map($categories, 'id', 'name');
+
+        $filterForm = new TasksFilterForm();
+        $filterForm->load(Yii::$app->request->get());
+
+        $selectedCategories = $filterForm->categories;
+        $isWithoutResponses = $filterForm->withoutResponse;
+        $isRemote = $filterForm->remote;
+        $period = (int) $filterForm->period;
+
+        $query = Task::find()
+            ->where(['status' => TaskLogic::STATUS_NEW])
+            ->with('city', 'category');
+
+        if (!is_null($selectedCategories)) {
+            $query->andWhere(['category_id' => $selectedCategories]);
+        }
+
+        if (!is_null($isWithoutResponses)) {
+            $taskIdsWithResponses = ArrayHelper::getColumn(Response::find()->asArray()->all(), 'task_id');
+            $query->andWhere(['not in', 'id', $taskIdsWithResponses]);
+        }
+
+        if (!is_null($isRemote)) {
+            $query->andWhere(['city_id' => null]);
+        }
+
+        if ($period !== 0) {
+            $query->andWhere("created_at > NOW() - INTERVAL :period HOUR", [
+                ':period' => $period
+            ]);
+        }
+
+        $tasksDataProvider = new ActiveDataProvider([
+            'query' => $query,
+            'pagination' => [
+                'pageSize' => 5,
+            ],
+            'sort' => [
+                'defaultOrder' => [
+                    'created_at' => SORT_DESC,
+                ]
+            ],
+        ]);
+
+        return $this->render('index', [
+            'tasksDataProvider' => $tasksDataProvider,
+            'filterForm' => $filterForm,
+            'categoriesList' => $categoriesList,
+        ]);
     }
 }
