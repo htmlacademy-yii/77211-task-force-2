7e60a81e77342bc86d11ac8ef7d3a8b04e862fc6
VK <belsynth@gmail.com>
2022-07-14 22:03:15 +0300

    Обновляет фикстуры для задания

--- fixtures/templates/task.php
+++ fixtures/templates/task.php
@@ -14,14 +14,14 @@ $citiesCount = City::find()->count();
 
 return [
     'customer_id' => $faker->numberBetween(1, $usersCount),
-    'executor_id' => $faker->numberBetween(1, $usersCount),
-    'status' => $faker->numberBetween(1, 5),
+    'executor_id' => $faker->optional(0.6)->numberBetween(1, $usersCount),
+    'status' => $faker->optional(0.6, 1)->numberBetween(2, 5),
     'title' => $faker->sentence(),
     'description' => $faker->text(),
     'category_id' => $faker->numberBetween(1, $categoriesCount),
     'budget' => $faker->numberBetween(500, 10000),
     'city_id' => $faker->optional($weight = 0.7)->numberBetween(1, $citiesCount),
     'coordinates' => null,
-    'created_at' => $faker->dateTimeBetween('-1 week', 'now')->format('Y-m-d H:i:s'),
+    'created_at' => $faker->dateTimeBetween('-36 hours', 'now')->format('Y-m-d H:i:s'),
     'deadline_at' => $faker->dateTimeBetween('now', '+1 week')->format('Y-m-d H:i:s'),
 ];
