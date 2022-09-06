4eeecffcf27e3d2eb43f9360e474b6867850c4bf
VK <belsynth@gmail.com>
2022-09-06 13:00:49 +0300

    Исправление по ТЗ. Профиль пользователя. Страница предназначена только для показа профилей исполнителей.

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -41,7 +41,7 @@ class UserController extends Controller
         $user = User::findOne($id);
         $currentUser = Yii::$app->user->identity;
 
-        if (!$user) {
+        if (!$user || $user->role === User::ROLE_CUSTOMER) {
             throw new NotFoundHttpException();
         }
 
